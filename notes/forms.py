37bec15bbad5b051af986000db05aad6b49c8a79
TarasovMaksim <55541599+MaksimTarasov@users.noreply.github.com>
2022-07-21 08:09:00 +0000

    part 1

--- notes/forms.py
+++ notes/forms.py
@@ -1,4 +1,7 @@
+from django import forms
+from .models import Notes
 
-class MainForm(forms.Form):
-    name = forms.CharField(initial='Name')
-    comment = forms.CharField(initial='Comment')
+class MainForm(forms.ModelForm):
+  class Meta:
+    model = Notes
+    fields = ['autor_of_note', 'title_of_note', 'note_text']
--- notes/views.py
+++ notes/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
-from . import models
+from . import models, forms
 # Create your views here.
 def index(request):
+  """
+  Вывод списка записей
+  """
   notes_list = models.Notes.objects.all()
   content = {'notes':'notes', 'notes_list':notes_list}
   return render(request, 'notes/index.html', content)
 
 def create_note(request):
+  """
+  СОздание 
+  """
   if request.method == 'POST':
-    mainform = forms.MainForm(request.POST)
-    if mainform.is_valid():
-      s = {'name':mainform.cleaned_data['name'],'comment':mainform.cleaned_data['comment']}
-    nam = mainform.cleaned_data['name']
-    com = mainform.cleaned_data['comment']
-    comment_db =  models.Comment.objects.create(name = nam, comment = com) 
+    pass
+    #mainform = forms.MainForm(request.POST)
+    # if mainform.is_valid():
+    #   s = {'name':mainform.cleaned_data['name'],'comment':mainform.cleaned_data['comment']}
+    # nam = mainform.cleaned_data['name']
+    # com = mainform.cleaned_data['comment']
+    # comment_db =  models.Comment.objects.create(name = nam, comment = com) 
   else:
-    content = {'create':''}
-    return render(request, 'create_notes.html', content)
+    create_form = forms.MainForm()
+    content = {'create_form':create_form}
+    return render(request, 'notes/create_note.html', content)
